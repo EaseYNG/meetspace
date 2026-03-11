365546ebaf8af600a9b0d98948590236fc143c55
Void <2604978804@qq.com>
2026-03-11 10:10:38 +0800

    first commit

---
+++ src/test/java/com/venus/meetspace/MeetspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venus.meetspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
